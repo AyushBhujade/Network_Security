86d670d1d5c90909760671e509ac87a8f7aa1296
Ayush bhujade <ayushbhujade2005@gmail.com>
2024-09-06 10:11:40 +0530

    validation config and entity added

--- network_Security/entity/artifacts.py
+++ network_Security/entity/artifacts.py
@@ -13,15 +13,21 @@ class DataIngestionArtifact:
     trained_file_path:str
     test_file_path:str
     
+    
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    valid_training_file_path:str
+    valid_test_file_path:str
+    invalid_train_file_path:str
+    invalid_ttest_file_path:str
+    drift_report_file_path:str
 @dataclass
 class DataTransformatiArtifact:
     def __init__(self):
         pass
     
-@dataclass
-class DataValidationArtifact:
-    def __init__(self):
-        pass
+
     
 @dataclass
 class  ModelEvaluationArtifact:
